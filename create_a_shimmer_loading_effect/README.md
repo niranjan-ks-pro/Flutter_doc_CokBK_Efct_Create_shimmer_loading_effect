cb39c4e48e0a85df032184c312b14570bef3efc9
Niranjan-KS <niranjan.ks.pro@gmail.com>
2023-07-27 15:23:06 +0530

    commit

---
+++ create_a_shimmer_loading_effect/README.md
@@ -0,0 +1,3 @@
+# create_a_shimmer_loading_effect
+
+A new Flutter project.
---
+++ create_a_shimmer_loading_effect/android/app/src/main/kotlin/com/example/create_a_shimmer_loading_effect/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.create_a_shimmer_loading_effect
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
